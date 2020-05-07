ec3af9f9820ef284c6c9c29aab057c874d204f31
manuel.garcia <mjgr0013@gmail.com>
2020-05-07 17:06:33 +0200

    Added fetch to posts endpoint

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,17 +1,29 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 const Blog = () => {
+  const [posts, setPosts] = useState([]);
+
+  function fetchPosts() {
+    axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+          setPosts(res.data.slice(0, 10))
+        })
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  })
+
   return (
       <div>
         <section className="Posts">
-          <Post/>
-          <Post/>
-          <Post/>
+          {posts.map(post => <Post key={post.id} {...post} />)}
         </section>
         <section>
           <FullPost/>
